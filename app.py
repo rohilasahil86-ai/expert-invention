6addb060d1e54be4b6f7fb3c04ac8a3ac790ec6c
Sahil <rohilasahil86@gmail.com>
2026-05-25 18:32:16 +0530

    fix port

--- app.py
+++ app.py
@@ -66,4 +66,6 @@ def chat():
     return jsonify({"reply": reply})
 
 if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
+ 
